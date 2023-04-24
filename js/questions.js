8d1bbedd353ea177d4160a12b93b9c66c28dce93
pranavkarthikb <pranavkarthik.cs20@bitsathy.ac.in>
2023-04-25 00:57:07 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is (2*3)+2",
+    answer: "8",
+    options: [
+      "6",
+      "4",
+      "8",
+      "60"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What type of stylesheets can be used in css",
+    answer: "all of the above",
+    options: [
+      "inline",
+      "internal",
+      "external",
+      "all of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "html means",
+    answer: "hyper text markup language",
+
+    options: [
+      "hyper Markup Language",
+      "hyper Multiple Language",
+      "eXTra Multi-Program Language",
+      "hyper text markup language"
+    ]
+  },
+  
+];
